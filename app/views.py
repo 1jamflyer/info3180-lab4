ca0335de7a6f14a06e160987f07dc66812d7603d
Jonathan Clarke <jonathan.clarke@mymona.uwi.edu>
2019-02-23 04:22:16 +0000

    INFO3180 Lab4

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ import os
 from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
+from .forms import UploadForm
 
 
 ###
@@ -32,8 +33,16 @@ def upload():
         abort(401)
 
     # Instantiate your form class
-
+    form = UploadForm()
     # Validate file upload on submit
+    if form.validate_on_submit():
+        photo = form.photo.data
+        filename = secure_filename(photo.filename)
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('File Saved', 'success')
+        return redirect(url_for('home'))
+    else:
+        return render_template('upload.html', form=form)
     if request.method == 'POST':
         # Get file data and save to your uploads folder
 
